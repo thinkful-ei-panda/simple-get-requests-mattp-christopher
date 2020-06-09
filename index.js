c68f2d07df27376a4f94a560d09e0319bc5a8f71
Matt <63121708+themlp101@users.noreply.github.com>
2020-06-09 16:08:27 -0400

    functions

---
+++ index.js
@@ -0,0 +1,37 @@
+'use-strict';
+
+function getDogImage(value) {
+  console.log(value)
+  fetch(`https://dog.ceo/api/breeds/image/random/${value}`)
+    .then(response => response.json())
+    .then(responseJson => 
+      displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  //replace the existing image with the new one
+  $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`
+  );
+  //display the results section
+  $('.results').removeClass('hidden');
+}
+
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const value = $('#js-input').val();
+    console.log(value)
+    getDogImage(value);
+  });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
+
+$(getDogImage);
